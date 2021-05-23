5f94ad1b7fe78ba6047a278dd9c1ed7dcc1d6165
soshika <soshika1010@gmail.com>
2021-05-23 15:39:33 +0430

    ping added

--- app/urls.go
+++ app/urls.go
@@ -1,8 +1,12 @@
 package app
 
+import (
+	"bitPay/controllers/ping"
+	"bitPay/controllers/user"
+)
 
 func urlPatterns() {
-	//router.GET("/ping", ping.Ping)
+	router.GET("/ping", ping.Ping)
 
-
+	router.GET("/users/balance", user.GetBalance)
 }
